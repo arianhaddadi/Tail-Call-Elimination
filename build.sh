200dc74a7a35e58a2b0ca0c2f6bb2b057e44d504
Arian <a94h.73@gmail.com>
2023-12-03 15:55:01 -0500

    Add README

--- build.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ../submission
-cp -r *.py c_files ../submission
-cd ../submission
-tar -czvf archive.tar.gz --exclude=.DS_Store *.py c_files
-rm -rf *.py c_files
-mv archive.tar.gz ../source_code.tar.gz
-cd ..
-rm -rf submission
